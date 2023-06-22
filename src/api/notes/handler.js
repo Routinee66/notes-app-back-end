1b89ac2b6c208e04ad36b6937df25098f0d29adb
Routinee66 <daniel.sinaga.ds@gmail.com>
2023-06-23 02:44:01 +0700

    data validation implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,9 @@
+const ClientError = require("../../exceptions/ClientError");
+
 class NoteHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
 
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -11,6 +14,7 @@ class NoteHandler {
 
   postNoteHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
 
       const noteId = this._service.addNote({ title, body, tags });
@@ -26,13 +30,23 @@ class NoteHandler {
       response.code(201);
       return response;
     } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
+        if(error instanceof ClientError){
+            const response = h.response({
+              status: 'fail',
+              message: error.message,
+            });
+            response.code(error.statusCode);
+            return response;
+        }
 
-      response.code(400);
-      return response;
+        const response = h.response({
+            status: 'fail',
+            message: 'Maaf, terjadi kegagalan pada server kami'
+        });
+
+        response.code(500);
+        console.error(error);
+        return response;
     }
   }
 
@@ -59,18 +73,30 @@ class NoteHandler {
         },
       };
     } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
+        if(error instanceof ClientError){
+            const response = h.response({
+              status: 'fail',
+              message: error.message,
+            });
+      
+            response.code(error.statusCode);
+            return response;
+        }
 
-      response.code(404);
-      return response;
+        const response = h.response({
+            status: 'fail',
+            message: 'Maaf, terjadi kegagalan pada server kami'
+        });
+
+        response.code(500);
+        console.error(error);
+        return response;
     }
   }
 
   putNoteByIdHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
 
       this._service.editNoteById(id, request.payload);
@@ -80,13 +106,24 @@ class NoteHandler {
         message: 'Catatan berhasil diperbarui',
       };
     } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
+        if(error instanceof ClientError){
+            const response = h.response({
+              status: 'fail',
+              message: error.message,
+            });
+      
+            response.code(error.statusCode);
+            return response;
+        }
 
-      response.code(404);
-      return response;
+        const response = h.response({
+            status: 'fail',
+            message: 'Maaf, terjadi kegagalan pada server kami'
+        });
+
+        response.code(500);
+        console.error(error);
+        return response;
     }
   }
 
@@ -100,13 +137,24 @@ class NoteHandler {
         message: 'Catatan berhasil dihapus',
       };
     } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
+        if(error instanceof ClientError){
+            const response = h.response({
+              status: 'fail',
+              message: error.message,
+            });
+      
+            response.code(error.statusCode);
+            return response;
+        }
 
-      response.code(404);
-      return response;
+        const response = h.response({
+            status: 'fail',
+            message: 'Maaf, terjadi kegagalan pada server kami'
+        });
+
+        response.code(500);
+        console.error(error);
+        return response;
     }
   }
 }
--- src/server.js
+++ src/server.js
@@ -2,31 +2,33 @@ const Hapi = require('@hapi/hapi');
 // const routes = require('./routes');
 const notes = require('./api/notes');
 const NotesServices = require('./service/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
-    const notesService = new NotesServices();
+  const notesService = new NotesServices();
 
-    const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    // server.route(routes);
+  // server.route(routes);
 
-    await server.register({
-        plugin: notes,
-        options: {
-            service: notesService,
-        }
-    });
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
